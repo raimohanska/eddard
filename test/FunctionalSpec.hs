6e4be3a7102a344401784c05b19f3a8b787f5a83
Juha Paananen <juha.paananen@reaktor.fi>
2011-11-07 23:27:15 +0200

    test query request too

--- test/FunctionalSpec.hs
+++ test/FunctionalSpec.hs
@@ -4,21 +4,29 @@ import Test.HUnit
 import qualified Main as Eddard
 import Control.Concurrent(forkIO, threadDelay, killThread)
 import Curl
+import Network.Curl(curlGetString)
 import Text.Regex.XMLSchema.String(match)
 import XmlMatch(clean)
 import Control.Exception(finally)
 
 functionalTests = TestList [
   postTest "Login request" "/" "<login><username>juha</username><password>secret</password></login>" "<login-reply>.*</login-reply>",
-  postTest "Non-matching request" "/" "lol" "error : no match"
+  postTest "Query request" "/" "<query><id>1</id><query-string>testing</query-string></query>" "<query-results><id>1</id></query-results>",
+  postTest "Non-matching request" "/" "lol" "error : no match",
+  getTest "Test results" "/results/1" ""
   ]
 
 rootUrl = "localhost:8000" 
 
 postTest :: String -> String -> String -> String -> Test
-postTest desc path request pattern = TestLabel desc $ TestCase $ withTestServer $ do
-    putStrLn $ "Sending : " ++ request
-    reply <- curlPostGetString (rootUrl ++ path) request
+postTest desc path request pattern = 
+  httpTest desc path (curlPostGetString (rootUrl ++ path) request) pattern
+
+getTest desc path pattern = httpTest desc path (curlGetString (rootUrl ++ path) []  >>= return . snd) pattern
+
+httpTest :: String -> String -> IO String -> String -> Test
+httpTest desc path request pattern = TestLabel desc $ TestCase $ withTestServer $ do
+    reply <- request
     putStrLn $ "Got reply : " ++ reply
     assertBool desc (match pattern (clean reply))
 
